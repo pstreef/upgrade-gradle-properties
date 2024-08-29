9f3646290166b212b8c2d52785406ec0ae5416b8
Peter Streef <p.streef@gmail.com>
2024-08-29 11:23:55 +0200

    first commit

---
+++ src/main/java/io/moderne/upgradegradleproperties/UpgradeGradlePropertiesApplication.java
@@ -0,0 +1,13 @@
+package io.moderne.upgradegradleproperties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpgradeGradlePropertiesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UpgradeGradlePropertiesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/moderne/upgradegradleproperties/UpgradeGradlePropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.moderne.upgradegradleproperties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpgradeGradlePropertiesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
